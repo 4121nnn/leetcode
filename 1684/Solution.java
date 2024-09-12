9e8d254f2d694fba0b7ba1f2ca8b11e3d420531c
4121nnn <4121nnn@gmail.com>
2024-09-12 06:42:15 +0300

    1684

--- 1684/Solution.java
+++ 1684/Solution.java
@@ -1,19 +1,28 @@
 class Solution {
-    public int countConsistentStrings(String allowed, String[] words) {
+    public int countConsistentStrings(String allowed, String[] words){
         boolean[] map = new boolean[26];
-        for(char c : allowed.toCharArray()) map[c-'a'] = true;
-        int count = 0;
-        for(String s : words){
-            if(isAllowed(s, map)) count++;
-        }
-        return count;
+        allowed.chars().forEach(c -> map[c - 'a'] = true);
+        return (int) Arrays.stream(words)
+                        .filter(s -> s.chars().allMatch(c -> map[c - 'a']))
+                        .count();
     }
-    private boolean isAllowed(String s, boolean[] map){
-        for(int i = 0; i < s.length(); i++){
-            if(!map[s.charAt(i)-'a']) return false;
-        }
-        return true;
-    }
+ 
+ 
+    // public int countConsistentStrings(String allowed, String[] words) {
+    //     boolean[] map = new boolean[26];
+    //     for(char c : allowed.toCharArray()) map[c-'a'] = true;
+    //     int count = 0;
+    //     for(String s : words){
+    //         if(isAllowed(s, map)) count++;
+    //     }
+    //     return count;
+    // }
+    // private boolean isAllowed(String s, boolean[] map){
+    //     for(int i = 0; i < s.length(); i++){
+    //         if(!map[s.charAt(i)-'a']) return false;
+    //     }
+    //     return true;
+    // }
 }
 // time O(n * m);
 // space O(1)
