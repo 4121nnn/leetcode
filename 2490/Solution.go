d53f6900ce39f80737716e4afd90ae48d9b357e2
4121nnn <4121nnn@gmail.com>
2024-11-02 08:02:43 +0300

    2490

---
+++ 2490/Solution.go
@@ -0,0 +1,13 @@
+func isCircularSentence(s string) bool {
+    if s[0] != s[len(s)-1] {
+        return false
+    }
+
+    for i, c := range s {
+        if c == ' ' && s[i-1] != s[i+1] {
+            return false
+        }
+    }
+
+    return true
+}
