7b07139fec6efede1f5436aca3c60a0e8319bf91
4121nnn <4121nnn@gmail.com>
2024-11-03 16:13:51 +0300

    0796

---
+++ 0796/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!= goal.length()) return false;
+        
+        int n = s.length();
+
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) == goal.charAt(0)){
+                if(s.substring(i).equals(goal.substring(0, n - i)) && s.substring(0, i).equals(goal.substring(n-i))){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
