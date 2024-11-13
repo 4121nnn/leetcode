cd6d0e9883d033ad5db10b255d5e786ea675af38
4121nnn <4121nnn@gmail.com>
2024-11-13 21:12:34 +0300

    2563

--- 2563/Solution.go
+++ 2563/Solution.go
@@ -1,15 +1,17 @@
 func countFairPairs(nums []int, lower int, upper int) int64 {
     sort.Ints(nums)
-    return find(nums, upper) - find(nums, lower -1)
+    return find(nums, upper) - find(nums, lower - 1)
 }
-func find(nums []int, upper int) int64 {
-    l, r, res := 0, len(nums) - 1, 0
-    for l < r {
-        for l < r && nums[l] + nums[r] > upper {
-            r--
+
+func find(nums []int, target int) int64 {
+    var res int64
+    for i, j := 0, len(nums) - 1; i < j; {
+        if nums[i] + nums[j] > target {
+            j--
+        } else {
+            res += int64(j - i)
+            i++
         }
-        res += r - l
-        l++
     }
-    return int64(res)
+    return res
 }
